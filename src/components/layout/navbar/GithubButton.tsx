c902c2c3f63130c000a5b8e1fcb5544463757058
Maxence <m.jaouan@gmail.com>
2025-02-21 07:53:19 +0100

    ♿️ Improve accessibility.

--- src/components/layout/navbar/GithubButton.tsx
+++ src/components/layout/navbar/GithubButton.tsx
@@ -3,6 +3,7 @@ import { IoLogoGithub } from "react-icons/io";
 export const GithubButton = () => (
   <div className="tooltip tooltip-bottom" data-tip={"Github"}>
     <a
+      aria-label="github"
       href="https://github.com/Jaouan/reviews"
       className="btn btn-ghost btn-circle"
     >
