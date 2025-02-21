fbb9cad42639b211ed3975e9ba3e668b37d63fb7
Maxence <m.jaouan@gmail.com>
2025-02-21 22:05:23 +0100

    💄 Improve privacy UI.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import { MdOutlinePrivacyTip } from "react-icons/md";
 
 export const Footer = () => (
   <footer className="footer footer-center p-4">
-    <p className="flex items-center text-base-content/50">
+    <p className="flex flex-wrap justify-center items-center text-base-content/50">
       <MdOutlinePrivacyTip />
       No data is collected, stored, or transmitted to external servers. All
       processing occurs locally in the browser, ensuring full data privacy and
