8ce28dd95f4f21a7d77d30a914745da9f6c8621a
Maxence <m.jaouan@gmail.com>
2025-02-26 01:00:22 +0100

    🗑️ Remove unused style.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => (
   <footer className="footer footer-center p-4">
-    <div className="transition-all text-xs flex flex-wrap flex-col justify-center items-center text-base-content/30 hover:text-base-content font-extralight">
+    <div className="transition-all text-xs text-base-content/30 hover:text-base-content font-extralight">
       No data is collected, stored, or transmitted to external servers.
       <br />
       All processing occurs locally in the browser, ensuring full data privacy
