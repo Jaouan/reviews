7a78e043d8f65468bc925db40b9eb96821306618
Maxence <m.jaouan@gmail.com>
2025-05-01 20:38:21 +0200

    🦶 Conditional disclaimer.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,20 @@
-export const Footer = () => (
-  <footer className="footer footer-center p-4">
-    <div className="transition-all text-xs text-base-content/30 hover:text-base-content font-extralight">
+import { DISCLAIMER } from "@/constants";
+
+const Disclaimer = () =>
+  DISCLAIMER && (
+    <div className="transition-all text-xs text-base-content/30 hover:text-base-content font-extralight inline">
       No data is collected, stored, or transmitted to external servers.
       <br />
       All processing occurs locally in the browser, ensuring full data privacy
-      and security.
+      and security.{" "}
+      <a href="https://github.com/Jaouan/reviews" className="underline">
+        You can also run it locally.
+      </a>
     </div>
+  );
+
+export const Footer = () => (
+  <footer className="footer footer-center p-4">
+    <Disclaimer />
   </footer>
 );
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,7 @@
 export const PROD = import.meta.env.PROD;
 export const VERSION = import.meta.env.BUILD_VERSION;
+export const DISCLAIMER = toBoolean(import.meta.env.VITE_DISCLAIMER);
+
+function toBoolean(value: string) {
+  return ["true", "1", "yes"].includes(value);
+}
